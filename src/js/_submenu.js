1ff4d500c1841e72c80cb1536a3d9ad199a8c644
Pavel <koz1tsk1y@mail.ru>
2024-08-10 20:40:47 +0300

    Добавить 3-й уровень меню

--- src/js/_submenu.js
+++ src/js/_submenu.js
@@ -1,8 +1,11 @@
-const menuParentItems = document.querySelectorAll('.nav__item-has-child');
-
-menuParentItems.forEach(item => {
-  item.addEventListener('click', (e) => {
-    e.preventDefault();
-    item.classList.toggle('active');
+const initSubmenu = (items) => {
+  items.forEach(item => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      item.parentElement.classList.toggle('active');
+    });
   });
-});
+}
+
+initSubmenu(document.querySelectorAll('.nav__item-has-child > a'));
+initSubmenu(document.querySelectorAll('.nav__submenu-item-has-child > a'));
